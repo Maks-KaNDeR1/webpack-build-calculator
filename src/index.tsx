e8f0fb33ac64c5b74b78d182c76b8d95d5ee9fa9
Maks-KaNDeR <makskander1@gmail.com>
2022-08-04 17:32:16 +0300

    react

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,10 @@
 import './index.css'
 import React from 'react'
-import { render } from 'react-dom'
+import ReactDOM from 'react-dom/client'
 import App from './App'
 
+const root = ReactDOM.createRoot(
+    document.getElementById('root') as HTMLElement
+);
 
-render(
-    <App />,
-    document.getElementById("root")
-)
+root.render(<App />)
